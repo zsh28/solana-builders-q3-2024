d95cccf0d0b2aa74f31117b14fe259a3deaf3639
zsh28 <zeeshan8210@outlook.com>
2024-07-30 23:43:16 +0300

    Challenge Create a Fungible Token

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,7 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+//commitment - The level of commitment desired when querying state
+import { createMint } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +11,45 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
+  try {
+    // Start here
+    //
+    {
+      /*
+        How to Create a Token
+        Creating tokens is done by creating what is called a "mint account".
+        This mint account is later used to mint tokens to a user's token account.
+     */
     }
-})()
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6
+    );
+    console.log(`successfully created mint: ${mint}`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
+
+// mint_address = "5N8rqmmhP8bwLWpHpCqc1BRHdWCwHgjKEzAhgke5UR6K"
+{
+  /*
+// Mint(
+    // Optional authority used to mint new tokens. The mint authority may only
+    be provided during mint creation. If no authority is present,
+    then the mint has a fixed supply and no further tokens may be minted.
+    pub mint_authority: Option<Pubkey>,
+    // Total supply of tokens
+    pub supply: u64,
+    // Number of base 10 digits to the right of the decimal place
+    pub decimals: u8,
+    // Is `true` if this structure has been initialized
+    pub is_initialized: bool,
+    // Optional authority to freeze the token accounts
+    pub freeze_authority: Option<Pubkey>,
+)
+*/
+}
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,6 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,18 +12,38 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("5N8rqmmhP8bwLWpHpCqc1BRHdWCwHgjKEzAhgke5UR6K");
 
 (async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+  try {
+    // Create an ATA
+    //why creata a ata? - An associated token account (ATA) is a token account that is associated with a particular wallet address.
+    // It is used to hold tokens that are associated with a particular wallet address.
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log(`Your ata is: ${ata.address.toBase58()}`);
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
+    // Mint to ATA
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair.publicKey,
+      BigInt(100) * token_decimals
+    );
+    console.log(`Your mint txid: ${mintTx}`);
+    {
+      /*
+        Your ata is: CBP2JETg7E2mbg17b4bW34ohm3jXsHPP3psNeux93Q4k
+        Your mint txid: 5t9ATaWSw8gnR6BFFNoCM5ZjxHay2JrN9FNjRea6CfkXrQRGwkpu69YhaDVgjqitGMLhwbQS5yTXNi1V4y8GjxNr
+    */
     }
-})()
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
